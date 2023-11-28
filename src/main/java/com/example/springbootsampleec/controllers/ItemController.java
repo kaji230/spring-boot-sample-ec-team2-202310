c0b52d2456cca079edbbae27dd6562ae93d62eba
user <user@192.168.10.108>
2023-11-28 21:45:23 +0900

    検索機能追加

--- src/main/java/com/example/springbootsampleec/controllers/ItemController.java
+++ src/main/java/com/example/springbootsampleec/controllers/ItemController.java
@@ -1,23 +1,24 @@
 package com.example.springbootsampleec.controllers;
  
+import java.util.ArrayList;
 import java.util.List;
-
-import javax.validation.Valid;
-
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+ 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute; 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.example.springbootsampleec.entities.Item;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import javax.validation.Valid;
+
 import com.example.springbootsampleec.entities.User;
+import com.example.springbootsampleec.entities.Item;
 import com.example.springbootsampleec.forms.ItemCreateForm;
 import com.example.springbootsampleec.forms.ItemEditForm;
 import com.example.springbootsampleec.services.ItemService;
