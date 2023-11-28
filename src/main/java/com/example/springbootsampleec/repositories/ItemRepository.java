a852d42c1597ea7af9cad2247c13af29c523c725
user <user@192.168.10.108>
2023-11-28 21:41:50 +0900

    検索機能追加

--- src/main/java/com/example/springbootsampleec/repositories/ItemRepository.java
+++ src/main/java/com/example/springbootsampleec/repositories/ItemRepository.java
@@ -1,13 +1,13 @@
 package com.example.springbootsampleec.repositories;
  
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
- 
-import com.example.springbootsampleec.entities.Item;
 
-import java.util.List;
+import com.example.springbootsampleec.entities.Item;
  
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
-
+	List<Item> findByNameContaining(String keyword);
 }
