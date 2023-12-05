5c4f3281fb0155c64dea29a6281642b7320d4f43
mitsu <mitsu@192.168.10.15>
2023-12-05 21:38:22 +0900

    修正

--- src/main/java/com/example/springbootsampleec/repositories/ShopRepository.java
+++ src/main/java/com/example/springbootsampleec/repositories/ShopRepository.java
@@ -1,5 +1,15 @@
 package com.example.springbootsampleec.repositories;
 
-public class ShopRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.springbootsampleec.entities.Shop;
+
+public interface ShopRepository extends JpaRepository<Shop, Long>{
+	List<Shop>findAll();
+	
+	@Query("SELECT s.id FROM Shop s")
+    List<Long> findAllShopIds();
 }
