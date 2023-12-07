08766a04f1615c5f8188b6ad0601656dedb96991
IwaiMasami <ponsan.3250@gmail.com>
2023-12-07 17:25:53 +0900

    できんー2

--- src/main/java/com/example/springbootsampleec/services/impl/CartServiceImpl.java
+++ src/main/java/com/example/springbootsampleec/services/impl/CartServiceImpl.java
@@ -3,6 +3,8 @@ package com.example.springbootsampleec.services.impl;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,37 +13,43 @@ import com.example.springbootsampleec.entities.Item;
 import com.example.springbootsampleec.entities.User;
 import com.example.springbootsampleec.repositories.CartRepository;
 import com.example.springbootsampleec.services.CartService;
-
 @Service
 public class CartServiceImpl implements CartService{
+	@Autowired
 	private final CartRepository cartRepository;
+	
+	@Autowired
+    private Environment environment;// 環境変数を使えるように。
 	
 	public CartServiceImpl(CartRepository cartRepository) {
         this.cartRepository = cartRepository;
-    }
-	//カート一覧
-	@Transactional(readOnly = true)
-    @Override
-    public List<Cart> findAll() {
-        return cartRepository.findAll();
-    }
-    //カートID取得
-    @Transactional(readOnly = true)
-    @Override
-    public Optional<Cart> findById(long id) {
-        return cartRepository.findById(id);
-    }
-    @Transactional
+    } 
+	
+	//カート一覧表示
+	 @Transactional(readOnly = true)
+	    @Override
+	    public List<Cart> findAll() {
+	        return cartRepository.findAll();
+	    }
+	 
+	//カートID取得
+	   @Transactional(readOnly = true)
+	    @Override
+	    public Optional<Cart> findById(long id) {
+	        return cartRepository.findById(id);
+	    }
+    
+    //カートに登録
+    @Transactional(readOnly = false)
     @Override
     public void register(User user, Item item, int amount) {
-    	// Cart エンティティの生成
-        Cart cart = new Cart(user, item, amount, null, null);
-        
+    	 // Cart エンティティの生成
+         Cart cart = new Cart(null,user, item, amount, null, null);        
          // Cart を保存
          cartRepository.saveAndFlush(cart);
        }
     
-    //削除
+    //アイテム削除
     @Transactional
     @Override
     public void delete(long id) {
