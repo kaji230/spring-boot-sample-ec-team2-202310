b5223ee8190967e49b37c9e045716d8dbadef048
mitsu <mitsu@192.168.10.15>
2023-12-10 21:56:04 +0900

    ログアウト可能。商品一覧（もっと見る）からカテゴリ別へ遷移可能

--- src/main/java/com/example/springbootsampleec/repositories/ItemRepository.java
+++ src/main/java/com/example/springbootsampleec/repositories/ItemRepository.java
@@ -18,4 +18,7 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 	//ランダムに商品を３件検索
 	@Query(value = "SELECT * FROM item ORDER BY RAND() LIMIT 3", nativeQuery = true)
 	List<Item> findRandom3Records();
+	
+	//商品の種類の同じ商品の一覧を検索
+	List<Item> findByType(int type);
 }
