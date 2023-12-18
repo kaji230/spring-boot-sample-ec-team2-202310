6620d621f45b23d09d3be4cf139033bef3034516
IwaiMasami <ponsan.3250@gmail.com>
2023-12-18 17:13:02 +0900

    モーダルのとこ

--- src/main/java/com/example/springbootsampleec/controllers/ItemController.java
+++ src/main/java/com/example/springbootsampleec/controllers/ItemController.java
@@ -54,6 +54,7 @@ public class ItemController {
         @AuthenticationPrincipal(expression = "user") User user,
         Model model
     ) {
+    	
     	List<Item> items = itemService.findAll();
     	List<Item> newItems = itemService.findTop3ByOrderByCreatedAtDesc();
     	List<Item> randomItems = itemService.findRandom3Records();
